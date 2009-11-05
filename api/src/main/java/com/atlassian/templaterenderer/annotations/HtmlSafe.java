2e01f32e1d0172ca63f308e717c95fe2d1c023cb
rwallace <rwallace@atlassian.com>
2009-11-05 18:30:55 +0000

    ATR-17 make the velocity template renderers backwards compat with the HtmlSafe annotations from the ATR API
    git-svn-id: https://studio.atlassian.com/svn/ATR/branches/atlassian-template-renderer-1.0.x@47217 2c54a935-e501-0410-bc05-97a93f6bca70

--- api/src/main/java/com/atlassian/templaterenderer/annotations/HtmlSafe.java
+++ api/src/main/java/com/atlassian/templaterenderer/annotations/HtmlSafe.java
@@ -11,11 +11,14 @@ import com.atlassian.templaterenderer.annotations.ReturnValueAnnotation;
 /**
  * Declares that this method returns an object that does not require encoding if it is printed to a HTML document via
  * its {@link Object#toString()} method
+ * 
+ * @deprecated use {@link com.atlassian.velocity.htmlsafe.HtmlSafe} instead
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 @ReturnValueAnnotation
 @CollectionInheritable
+@Deprecated
 public @interface HtmlSafe
 {
 }
